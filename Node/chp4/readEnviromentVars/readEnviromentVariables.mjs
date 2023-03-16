2c327908a317e5ea73b8e11785ba39542a81f05a
manurengar <manuelrentero@outlook.com>
2023-03-16 11:25:17 +0100

    change

--- Node/chp4/readEnviromentVars/readEnviromentVariables.mjs
+++ Node/chp4/readEnviromentVars/readEnviromentVariables.mjs
@@ -1,6 +1,9 @@
 // If we initiate the program with the following
 // node program.mjs --env=debug
-console.log(process.argv[0]);
-console.log(process.argv[1]);
-console.log(process.argv[2]);
-console.log(process.argv[3]);
+
+console.log(process.argv[0]); ///home/pi/.nvm/versions/node/v16.19.1/bin/node
+console.log(process.argv[1]); // /home/pi/node/Node/chp4/readEnviromentVars/readEnviromentVariables.mjs
+console.log(process.argv[2]); // --env=debug
+
+// see versions of current libraries on json format
+console.log(process.versions);
