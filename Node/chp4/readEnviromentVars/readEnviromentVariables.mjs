2e840f7c9ed36178a7d64a24e2fc295e87b95e1a
manurengar <manuelrentero@outlook.com>
2023-03-16 11:37:01 +0100

    new

--- Node/chp4/readEnviromentVars/readEnviromentVariables.mjs
+++ Node/chp4/readEnviromentVars/readEnviromentVariables.mjs
@@ -1,5 +1,6 @@
 // If we initiate the program with the following
 // node program.mjs --env=debug
+import process from  'process';
 
 console.log(process.argv[0]); ///home/pi/.nvm/versions/node/v16.19.1/bin/node
 console.log(process.argv[1]); // /home/pi/node/Node/chp4/readEnviromentVars/readEnviromentVariables.mjs
@@ -7,3 +8,11 @@ console.log(process.argv[2]); // --env=debug
 
 // see versions of current libraries on json format
 console.log(process.versions);
+
+// get current user
+console.log(process.geteuid());
+
+// set user id
+console.log(process.seteuid('1001'));
+
+console.log(process.geteuid());
